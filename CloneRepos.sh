b9ccffa42083fae17d48172d0c22eb27bd103028
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-03-21 22:10:15 +0900

    リファクタリング

--- CloneRepos.sh
+++ CloneRepos.sh
@@ -2,15 +2,8 @@
 
 script_dir=$(readlink -f "$(dirname "$0")")
 
-pushd "${HOME}"
-if [[ ! -e Program ]] ; then
-    if [[ -d '/ext-hdd/data' ]] ; then
-        mkdir -p "/ext-hdd/data/Program"
-        ln -s    "/ext-hdd/data/Program"
-    else
-        mkdir -p Program
-    fi
-fi
+/bin/bash -xue "${script_dir}/setup-directory.sh"
+
 pushd "${HOME}/Program"
 
 ##  Both HG and GIT Repository
